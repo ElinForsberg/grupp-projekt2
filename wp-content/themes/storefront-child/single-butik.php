841b04440aabcb1de1122adb2e606da6b8577f36
r-4u-1 <114141854+r-4u-1@users.noreply.github.com>
2023-04-27 21:15:06 +0200

    new custom fields fore store and styling

--- wp-content/themes/storefront-child/single-butik.php
+++ wp-content/themes/storefront-child/single-butik.php
@@ -1,12 +1,23 @@
 <?php
 
-get_header( 'shop' ); 
+get_header('shop');
 
 the_post();
-the_title();
-$store_adress = get_post_meta(get_the_ID(), 'adress', true);
-?><h3><?php echo $store_adress ?></h3><?php
 
-the_content(); 
-
+?>
+<div class="single-store-info-container">
+    <?php
+    $store_adress = get_post_meta(get_the_ID(), 'adress', true);
+    $store_tel = get_post_meta(get_the_ID(), 'tel', true);
+    $store_email = get_post_meta(get_the_ID(), 'e-mail', true);
+    the_title();
+    ?>
+    <h5><?php echo $store_adress ?></h5>
+    <h5>tel. <?php echo $store_tel ?></h5>
+    <h5><a href="<?php echo $store_email ?>"> <?php echo $store_email ?></a></h5>
+<?php
+    the_content();
+?>
+</div>
+<?php
 get_footer();
